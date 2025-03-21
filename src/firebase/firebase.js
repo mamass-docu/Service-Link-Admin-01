1afc6a497db4bcf83908f51a73e2086609f682b3
unknown <eustaquioedmar3@gmail.com>
2025-03-21 13:20:03 +0800

    03212025

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,14 +6,24 @@ import {
   browserLocalPersistence,
 } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBOndErzcVbIpf6NWvacPQuv35rJnJ8VNs",
+//   authDomain: "servicelink-d960c.firebaseapp.com",
+//   projectId: "servicelink-d960c",
+//   storageBucket: "servicelink-d960c.firebasestorage.app",
+//   messagingSenderId: "780467063427",
+//   appId: "1:780467063427:web:65e0e758052f85d0cca9b6",
+//   databaseURL: "https://servicelink-d960c-default-rtdb.firebaseio.com",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBOndErzcVbIpf6NWvacPQuv35rJnJ8VNs",
-  authDomain: "servicelink-d960c.firebaseapp.com",
-  projectId: "servicelink-d960c",
-  storageBucket: "servicelink-d960c.firebasestorage.app",
-  messagingSenderId: "780467063427",
-  appId: "1:780467063427:web:65e0e758052f85d0cca9b6",
-  databaseURL: "https://servicelink-d960c-default-rtdb.firebaseio.com", // Add this if you have Realtime Database
+  apiKey: "AIzaSyClDXWMCEVWQuamBLVKnQdXG1n4PlpiJFs",
+  authDomain: "service-link-d7b4e.firebaseapp.com",
+  projectId: "service-link-d7b4e",
+  storageBucket: "service-link-d7b4e.firebasestorage.app",
+  messagingSenderId: "849305888050",
+  appId: "1:849305888050:web:f9e38748c2c47e22aaa32b",
+  databaseURL: "https://service-link-d7b4e-default-rtdb.firebaseio.com",
 };
 
 const app = initializeApp(firebaseConfig);
